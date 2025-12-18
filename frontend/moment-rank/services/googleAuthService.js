f5983fe0471e0dd3754438c7861979964b30a75f
Arturas Kovtun <mardaca8@gmail.com>
2025-12-18 14:58:33 +0200

    google login fix

--- frontend/moment-rank/services/googleAuthService.js
+++ frontend/moment-rank/services/googleAuthService.js
@@ -7,8 +7,8 @@ const API_URL = BASE_URL;
 
 // Configure Google Sign-In
 GoogleSignin.configure({
-  iosClientId: '734920707150-emfei8cufj25tlk703s4l4gmcvgnkcfh.apps.googleusercontent.com',
-  webClientId: '734920707150-th7t3n3n8ah9ftsfta2m1id4opm8d4gm.apps.googleusercontent.com',
+  iosClientId: '755918280086-cnt76mvrp4j7323c0qiujelule7mbn6k.apps.googleusercontent.com',
+  webClientId: '755918280086-lg9aqpg6o5pb9dq4ra5didcd3vbfjste.apps.googleusercontent.com',
   offlineAccess: true,
   scopes: ['profile', 'email'],
 });

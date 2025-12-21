0178419b21923d1dbbabdeecf34f79502aa1378e
Asmedis <martynas.masionis@gmail.com>
2025-12-21 14:00:36 +0200

    Change config.js

--- frontend/moment-rank/Config.js
+++ frontend/moment-rank/Config.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://172.20.10.5:5000';
-//const BASE_URL = 'https://momentrank.vr.lt';
+//const BASE_URL = 'http://172.20.10.5:5000';
+const BASE_URL = 'https://momentrank.vr.lt';
 
 export default BASE_URL;
